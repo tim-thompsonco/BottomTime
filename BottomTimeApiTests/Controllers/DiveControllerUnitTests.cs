0343e14e0186d29cdcbf3acc1d6916e2c17d929e
Tim Thompson <scubatim84@gmail.com>
2021-06-02 06:50:50 -0600

    Add mock of dive repository, add unit test for DiveController, remove dummy unit tests

---
+++ BottomTimeApiTests/Controllers/DiveControllerUnitTests.cs
@@ -0,0 +1,29 @@
+﻿using BottomTimeApi.Controllers;
+using BottomTimeApi.Models;
+using BottomTimeApiTests.DataAccess;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace BottomTimeApiTests.Controllers {
+	[TestClass]
+	public class DiveControllerUnitTests {
+		[TestMethod]
+		public async Task GetDivesUnitTestAsync() {
+			DiveRepositoryMock repository = new DiveRepositoryMock();
+			DiveController controller = new DiveController(repository);
+
+			ActionResult<List<Dive>> testActionResult = await controller.GetDivesAsync();
+			OkObjectResult testResponse = testActionResult.Result as OkObjectResult;
+			List<Dive> testValues = testResponse.Value as List<Dive>;
+
+			Assert.IsNotNull(testResponse);
+			Assert.AreEqual(2, testValues.Count);
+			Assert.AreEqual(repository.TestDives[0].Id, testValues[0].Id);
+			Assert.AreEqual(repository.TestDives[0].DiveSite, testValues[0].DiveSite);
+			Assert.AreEqual(repository.TestDives[1].Id, testValues[1].Id);
+			Assert.AreEqual(repository.TestDives[1].DiveSite, testValues[1].DiveSite);
+		}
+	}
+}
---
+++ BottomTimeApiTests/DataAccess/DiveRepositoryMock.cs
@@ -0,0 +1,38 @@
+﻿using BottomTimeApi.DataAccess;
+using BottomTimeApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace BottomTimeApiTests.DataAccess {
+	public class DiveRepositoryMock : IDiveRepository {
+		public List<Dive> TestDives { get; set; }
+
+		public DiveRepositoryMock() {
+			TestDives = new List<Dive> {
+				new Dive{Id=1, DiveSite = "Test site one"},
+				new Dive{Id=2, DiveSite = "Test site two"}
+			};
+		}
+
+		public Task AddDiveAsync(Dive dive) {
+			throw new NotImplementedException();
+		}
+
+		public Task DeleteDiveAsync(Dive dive) {
+			throw new NotImplementedException();
+		}
+
+		public Task<Dive> GetDiveByIdAsync(int id) {
+			throw new NotImplementedException();
+		}
+
+		public async Task<IEnumerable<Dive>> GetDivesAsync() {
+			return TestDives;
+		}
+
+		public Task UpdateDiveAsync(Dive dive) {
+			throw new NotImplementedException();
+		}
+	}
+}
