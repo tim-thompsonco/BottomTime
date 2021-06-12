569d91a4373638dc741b7d2e1ab306d2f7fa181a
Tim Thompson <scubatim84@gmail.com>
2021-06-11 20:50:00 -0600

    Adjust types to match 1:1 with what Postgres package will map for .NET, remove lists for now since they will require additional tables

--- BottomTimeApi/Models/Dive.cs
+++ BottomTimeApi/Models/Dive.cs
@@ -1,48 +1,43 @@
 ﻿using BottomTimeApi.Models.Enums;
 using System;
-using System.Collections.Generic;
 
 namespace BottomTimeApi.Models {
 	public class Dive {
-		public ushort Number { get; set; }
+		public short Number { get; set; }
 		public DateTime Date { get; set; }
 		public string Location { get; set; }
 		public string DiveSite { get; set; }
-		public DateTime TimeIn { get; set; }
-		public ushort StartAirPressure { get; set; }
-		public ushort EndAirPressure { get; set; }
+		public TimeSpan TimeIn { get; set; }
+		public short StartAirPressure { get; set; }
+		public short EndAirPressure { get; set; }
 		public PressureType PressureType { get; set; }
-		public List<WeatherAttributes> WeatherAttributes { get; set; }
-		public List<DiveConditions> DiveConditions { get; set; }
-		public List<DiveBottomAttributes> DiveBottomAttributes { get; set; }
 		public bool WearWetSuit { get; set; }
 		public WetSuitType? WetSuitType { get; set; }
-		public byte? WetSuitThickness { get; set; }
+		public short? WetSuitThickness { get; set; }
 		public bool WearDrySuit { get; set; }
 		public DrySuitType? DrySuitType { get; set; }
-		public byte? DrySuitNumOfLiners { get; set; }
-		public ushort MaxDepth { get; set; }
-		public ushort AvgDepth { get; set; }
-		public DateTime? SurfaceIntervalTime { get; set; }
-		public DateTime BottomTime { get; set; }
+		public short? DrySuitNumOfLiners { get; set; }
+		public short MaxDepth { get; set; }
+		public short AvgDepth { get; set; }
+		public TimeSpan? SurfaceIntervalTime { get; set; }
+		public TimeSpan BottomTime { get; set; }
 		public bool DidSafetyStop { get; set; }
-		public DateTime? SafetyStopTime { get; set; }
-		public char? PressureGroupStart { get; set; }
-		public char? PressureGroupEnd { get; set; }
-		public DateTime? ResidualNitrogenTime { get; set; }
-		public DateTime? AbsoluteBottomTime { get; set; }
-		public DateTime? TotalBottomTime { get; set; }
-		public sbyte WaterTemperature { get; set; }
+		public TimeSpan? SafetyStopTime { get; set; }
+		public string PressureGroupStart { get; set; }
+		public string PressureGroupEnd { get; set; }
+		public TimeSpan? ResidualNitrogenTime { get; set; }
+		public TimeSpan? AbsoluteBottomTime { get; set; }
+		public TimeSpan? TotalBottomTime { get; set; }
+		public short WaterTemperature { get; set; }
 		public TemperatureType TemperatureType { get; set; }
-		public byte Visibility { get; set; }
+		public short Visibility { get; set; }
 		public VisibilityType VisibilityType { get; set; }
-		public byte Weight { get; set; }
+		public short Weight { get; set; }
 		public WeightType WeightType { get; set; }
-		public byte TankSize { get; set; }
+		public short TankSize { get; set; }
 		public TankType TankType { get; set; }
 		public TankPressureType TankPressureType { get; set; }
 		public float SurfaceAirConsumption { get; set; }
-		public List<string> AquaticLifeObserved { get; set; }
 		public string DiveComments { get; set; }
 		public string DiveBuddy { get; set; }
 		public string DiveBuddyCertificationNumber { get; set; }
