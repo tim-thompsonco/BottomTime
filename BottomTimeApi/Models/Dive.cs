591886a2aa21327c21ade1f2f847e7e156d1ee59
Tim Thompson <scubatim84@gmail.com>
2021-06-11 20:15:46 -0600

    Optimize data types for Dive model to improve memory efficiency

--- BottomTimeApi/Models/Dive.cs
+++ BottomTimeApi/Models/Dive.cs
@@ -4,27 +4,27 @@ using System.Collections.Generic;
 
 namespace BottomTimeApi.Models {
 	public class Dive {
-		public int Number { get; set; }
+		public ushort Number { get; set; }
 		public DateTime Date { get; set; }
 		public string Location { get; set; }
 		public string DiveSite { get; set; }
 		public DateTime TimeIn { get; set; }
-		public int StartAirPressure { get; set; }
-		public int EndAirPressure { get; set; }
+		public ushort StartAirPressure { get; set; }
+		public ushort EndAirPressure { get; set; }
 		public PressureType PressureType { get; set; }
 		public List<WeatherAttributes> WeatherAttributes { get; set; }
 		public List<DiveConditions> DiveConditions { get; set; }
 		public List<DiveBottomAttributes> DiveBottomAttributes { get; set; }
 		public bool WearWetSuit { get; set; }
 		public WetSuitType? WetSuitType { get; set; }
-		public int? WetSuitThickness { get; set; }
+		public byte? WetSuitThickness { get; set; }
 		public bool WearDrySuit { get; set; }
 		public DrySuitType? DrySuitType { get; set; }
-		public int? DrySuitNumOfLiners { get; set; }
-		public int MaxDepth { get; set; }
-		public int AvgDepth { get; set; }
+		public byte? DrySuitNumOfLiners { get; set; }
+		public ushort MaxDepth { get; set; }
+		public ushort AvgDepth { get; set; }
 		public DateTime? SurfaceIntervalTime { get; set; }
-		public int BottomTime { get; set; }
+		public DateTime BottomTime { get; set; }
 		public bool DidSafetyStop { get; set; }
 		public DateTime? SafetyStopTime { get; set; }
 		public char? PressureGroupStart { get; set; }
@@ -32,13 +32,13 @@ namespace BottomTimeApi.Models {
 		public DateTime? ResidualNitrogenTime { get; set; }
 		public DateTime? AbsoluteBottomTime { get; set; }
 		public DateTime? TotalBottomTime { get; set; }
-		public int WaterTemperature { get; set; }
+		public sbyte WaterTemperature { get; set; }
 		public TemperatureType TemperatureType { get; set; }
-		public int Visibility { get; set; }
+		public byte Visibility { get; set; }
 		public VisibilityType VisibilityType { get; set; }
-		public int Weight { get; set; }
+		public byte Weight { get; set; }
 		public WeightType WeightType { get; set; }
-		public int TankSize { get; set; }
+		public byte TankSize { get; set; }
 		public TankType TankType { get; set; }
 		public TankPressureType TankPressureType { get; set; }
 		public float SurfaceAirConsumption { get; set; }
