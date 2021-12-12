01acb484bc32cf532a5ff00638d69c455f446892
Tim Thompson <scubatim84@gmail.com>
2021-12-12 11:11:45 -0700

    Add developer exception page when using local dev env

--- BottomTimeApi/Startup.cs
+++ BottomTimeApi/Startup.cs
@@ -1,8 +1,10 @@
 using BottomTimeApi.Extensions;
 using BottomTimeApi.Middleware;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 namespace BottomTimeApi {
 	public class Startup {
@@ -16,8 +18,12 @@ namespace BottomTimeApi {
 			services.AddApplicationServices(_config);
 		}
 
-		public void Configure(IApplicationBuilder app) {
+		public void Configure(IApplicationBuilder app, IWebHostEnvironment env) {
 			app.UseMiddleware<ExceptionMiddleware>();
+
+			if (env.IsDevelopment()) {
+				app.UseDeveloperExceptionPage();
+			}
 
 			app.UseSwagger();
 			app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "BottomTimeApi v1"));
