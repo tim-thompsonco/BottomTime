98cf5bd74d110f7dcdde53105afe34a43496fb42
Tim Thompson <scubatim84@gmail.com>
2021-12-12 11:46:24 -0700

    Combine dive and dive post models to remove redundant code, add default values to DivePost model for Swagger example schema

--- BottomTimeApi/Models/Dive.cs
+++ BottomTimeApi/Models/Dive.cs
@@ -2,44 +2,7 @@
 using System;
 
 namespace BottomTimeApi.Models {
-	public class Dive {
+	public class Dive : DivePost {
 		public int Id { get; set; }
-		public short Number { get; set; }
-		public DateTime Date { get; set; }
-		public string Location { get; set; }
-		public string DiveSite { get; set; }
-		public short StartAirPressure { get; set; }
-		public short EndAirPressure { get; set; }
-		public PressureType PressureType { get; set; }
-		public bool WearWetSuit { get; set; }
-		public WetSuitType? WetSuitType { get; set; }
-		public short? WetSuitThickness { get; set; }
-		public bool WearDrySuit { get; set; }
-		public DrySuitType? DrySuitType { get; set; }
-		public short? DrySuitNumOfLiners { get; set; }
-		public short MaxDepth { get; set; }
-		public short? AvgDepth { get; set; }
-		public TimeSpan? SurfaceIntervalTime { get; set; }
-		public TimeSpan BottomTime { get; set; }
-		public bool DidSafetyStop { get; set; }
-		public TimeSpan? SafetyStopTime { get; set; }
-		public string PressureGroupStart { get; set; }
-		public string PressureGroupEnd { get; set; }
-		public TimeSpan? ResidualNitrogenTime { get; set; }
-		public TimeSpan? AbsoluteBottomTime { get; set; }
-		public TimeSpan? TotalBottomTime { get; set; }
-		public short WaterTemperature { get; set; }
-		public TemperatureType TemperatureType { get; set; }
-		public short Visibility { get; set; }
-		public VisibilityType VisibilityType { get; set; }
-		public short Weight { get; set; }
-		public WeightType WeightType { get; set; }
-		public short TankSize { get; set; }
-		public TankType TankType { get; set; }
-		public TankPressureType TankPressureType { get; set; }
-		public string DiveComments { get; set; }
-		public string DiveBuddy { get; set; }
-		public string DiveBuddyCertificationNumber { get; set; }
-		public DiveBuddyCertificationType DiveBuddyCertificationType { get; set; }
 	}
 }
--- BottomTimeApi/Models/DivePost.cs
+++ BottomTimeApi/Models/DivePost.cs
@@ -1,13 +1,19 @@
 using BottomTimeApi.Enums;
 using System;
+using System.ComponentModel;
 
 namespace BottomTimeApi.Models {
 	public class DivePost {
+		[DefaultValue(100)]
 		public short Number { get; set; }
 		public DateTime Date { get; set; }
+		[DefaultValue("Sample dive location")]
 		public string Location { get; set; }
+		[DefaultValue("Sample dive site")]
 		public string DiveSite { get; set; }
+		[DefaultValue(3000)]
 		public short StartAirPressure { get; set; }
+		[DefaultValue(1000)]
 		public short EndAirPressure { get; set; }
 		public PressureType PressureType { get; set; }
 		public bool WearWetSuit { get; set; }
@@ -16,28 +22,45 @@ namespace BottomTimeApi.Models {
 		public bool WearDrySuit { get; set; }
 		public DrySuitType? DrySuitType { get; set; }
 		public short? DrySuitNumOfLiners { get; set; }
+		[DefaultValue(45)]
 		public short MaxDepth { get; set; }
+		[DefaultValue(30)]
 		public short AvgDepth { get; set; }
+		[DefaultValue("00:00:00")]
 		public TimeSpan? SurfaceIntervalTime { get; set; }
+		[DefaultValue("00:45:00")]
 		public TimeSpan BottomTime { get; set; }
 		public bool DidSafetyStop { get; set; }
+		[DefaultValue("00:03:00")]
 		public TimeSpan? SafetyStopTime { get; set; }
+		[DefaultValue("A")]
 		public string PressureGroupStart { get; set; }
+		[DefaultValue("Z")]
 		public string PressureGroupEnd { get; set; }
+		[DefaultValue("00:00:00")]
 		public TimeSpan? ResidualNitrogenTime { get; set; }
+		[DefaultValue("00:00:00")]
 		public TimeSpan? AbsoluteBottomTime { get; set; }
+		[DefaultValue("00:00:00")]
 		public TimeSpan? TotalBottomTime { get; set; }
+		[DefaultValue(50)]
 		public short WaterTemperature { get; set; }
 		public TemperatureType TemperatureType { get; set; }
+		[DefaultValue(20)]
 		public short Visibility { get; set; }
 		public VisibilityType VisibilityType { get; set; }
+		[DefaultValue(20)]
 		public short Weight { get; set; }
 		public WeightType WeightType { get; set; }
+		[DefaultValue(80)]
 		public short TankSize { get; set; }
 		public TankType TankType { get; set; }
 		public TankPressureType TankPressureType { get; set; }
+		[DefaultValue("What a great dive!")]
 		public string DiveComments { get; set; }
+		[DefaultValue("Sample Dive Buddy")]
 		public string DiveBuddy { get; set; }
+		[DefaultValue("PADI001")]
 		public string DiveBuddyCertificationNumber { get; set; }
 		public DiveBuddyCertificationType DiveBuddyCertificationType { get; set; }
 	}
